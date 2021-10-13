27a2f7560d94746369370ce3e74c1b4e0cca1466
Benjamin Prieur <benjaminp@voodoo.com>
2021-10-13 16:51:07 +0200

    Simple P2P application

---
+++ p2p/go.mod
@@ -0,0 +1,12 @@
+module github.com/BenjaminPrieur/GoPOCs/p2p
+
+go 1.16
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/ipfs/go-log v1.0.5
+	github.com/libp2p/go-libp2p v0.15.1
+	github.com/libp2p/go-libp2p-core v0.9.0
+	github.com/libp2p/go-libp2p-peerstore v0.2.8
+	github.com/multiformats/go-multiaddr v0.4.0
+)
---
+++ p2p/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"log"
+	"sync"
+	"time"
+
+	golog "github.com/ipfs/go-log"
+	host "github.com/libp2p/go-libp2p-core/host"
+	peer "github.com/libp2p/go-libp2p-core/peer"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/multiformats/go-multiaddr"
+)
+
+var (
+	// Blockchain is a series of validated Blocks
+	Blockchain []Block
+
+	mutex = &sync.Mutex{}
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	t := time.Now()
+	genesisBlock := Block{}
+	genesisBlock = Block{0, t.String(), 0, calculateHash(genesisBlock), ""}
+
+	Blockchain = append(Blockchain, genesisBlock)
+
+	// LibP2P code uses golog to log messages. They log with different
+	// string IDs (i.e. "swarm"). We can control the verbosity level for
+	// all loggers with:
+	golog.SetAllLoggers(golog.LevelInfo) // Change to DEBUG for extra info
+
+	// Parse options from the command line
+	listenF := flag.Int("l", 0, "wait for incoming connections")
+	target := flag.String("d", "", "target peer to dial")
+	secio := flag.Bool("secio", false, "enable secio")
+	seed := flag.Int64("seed", 0, "set random seed for id generation")
+	flag.Parse()
+
+	if *listenF == 0 {
+		log.Fatal("Please provide a port to bind on with -l")
+	}
+
+	// Make a host that listens on the given multiaddress
+	h, err := makeBasicHost(*listenF, *secio, *seed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set a stream handler on host A. /p2p/1.0.0 is
+	// a user-defined protocol name.
+	h.SetStreamHandler("/p2p/1.0.0", handleStream)
+
+	if *target != "" {
+
+		rw, err := startPeerAndConnect(ctx, h, *target)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Create a thread to read and write data.
+		go writeData(rw)
+		go readData(rw)
+	}
+
+	select {} // hang forever
+}
+
+func startPeerAndConnect(ctx context.Context, h host.Host, target string) (*bufio.ReadWriter, error) {
+	log.Println("This node's multiaddresses:")
+	for _, la := range h.Addrs() {
+		log.Printf(" - %v\n", la)
+	}
+	log.Println()
+
+	// Turn the destination into a multiaddr.
+	maddr, err := multiaddr.NewMultiaddr(target)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Extract the peer ID from the multiaddr.
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Add the destination's peer multiaddress in the peerstore.
+	// This will be used during connection and stream creation by libp2p.
+	h.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+	// Start a stream with the destination.
+	// Multiaddress of the destination peer is fetched from the peerstore using 'peerId'.
+	s, err := h.NewStream(context.Background(), info.ID, "/p2p/1.0.0")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Println("Established connection to destination")
+
+	// Create a buffered stream so that read and writes are non blocking.
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+	return rw, nil
+}
